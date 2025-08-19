e9f8842dacac8861d8c802a3bc25df47dd64ed69
annu32 <167179726+annu32@users.noreply.github.com>
2025-08-19 11:24:57 +0530

    updated variable live url

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-const live_url="mongodb+srv://bhadoriyaanurag88:Anurag32@cluster0.ityj6az.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const LIVE_URL="mongodb+srv://bhadoriyaanurag88:Anurag32@cluster0.ityj6az.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectdb=()=>{
     return mongoose.connect(process.env.live_url)
